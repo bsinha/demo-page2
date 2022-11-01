b52c0a98abc365068772ded2099aad5222bf7e3d
Pooja4183 <poojatomar.jt@gmail.com>
2022-11-01 07:51:07 +0530

    added cors on weback

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react");
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa-react');
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "bipsin",
-    projectName: "demo-page2",
+    orgName: 'bipsin',
+    projectName: 'demo-page2',
     webpackConfigEnv,
     argv,
   });
 
   return merge(defaultConfig, {
+    devServer: {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
     // modify the webpack config however you'd like to by adding to this object
   });
 };
